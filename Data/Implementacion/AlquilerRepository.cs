8222913ec413412fa35874f458496b7bcb7862fd
MrtNg <marting_ng_96@hotmail.com>
2019-04-23 18:23:51 -0500

    Cambios a la implementacion de Data

--- Data/Implementacion/AlquilerRepository.cs
+++ Data/Implementacion/AlquilerRepository.cs
@@ -4,34 +4,145 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entity;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 namespace Data.Implementacion
 {
     public class AlquilerRepository : IAlquilerRepository
     {
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("delete from Alquiler where CAlquiler='" + id + "'",con);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
 
         public List<Alquiler> FindAll()
         {
-            throw new NotImplementedException();
+            List<Alquiler> alquileres = new List<Alquiler>();
+            try
+            {
+                using(var con=new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from Alquiler ",con);
+                    var dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Alquiler alquiler = new Alquiler();
+                        alquiler.CGrupo.CGrupo = Convert.ToInt32(dr["CGrupo"]);
+                        alquiler.CCancha.CCancha = Convert.ToInt32(dr["CCancha"]);
+                        alquiler.NumHoras = Convert.ToInt32(dr["NumHoras"]);
+                        alquiler.MDescuento = Convert.ToInt32(dr["MDescuento"]);
+                        alquiler.MTotal = Convert.ToInt32(dr["MTotal"]);
+                        alquiler.FPagado = Convert.ToBoolean(dr["FPagado"]);
+                        alquiler.CHorario.CHorario = Convert.ToInt32(dr["CHorario"]);
+                        alquileres.Add(alquiler);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return alquileres;
         }
 
         public Alquiler FindById(int? id)
         {
-            throw new NotImplementedException();
+            Alquiler alquiler = new Alquiler(); 
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from Alquiler where id='"+id+"'",con);
+                    var dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        alquiler.CGrupo.CGrupo = Convert.ToInt32(dr["CGrupo"]);
+                        alquiler.CCancha.CCancha = Convert.ToInt32(dr["CCancha"]);
+                        alquiler.NumHoras = Convert.ToInt32(dr["NumHoras"]);
+                        alquiler.MDescuento = Convert.ToInt32(dr["MDescuento"]);
+                        alquiler.MTotal = Convert.ToInt32(dr["MTotal"]);
+                        alquiler.FPagado = Convert.ToBoolean(dr["FPagado"]);
+                        alquiler.CHorario.CHorario = Convert.ToInt32(dr["CHorario"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return alquiler;
         }
 
         public bool Insertar(Alquiler t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("insert into Alquiler values(@CGrupo,@CCancha,@NumHoras,@MDescuento,@MTotal,@FPagado,@CHorario)",con);
+                    cmd.Parameters.AddWithValue("@CGrupo", t.CGrupo.CGrupo);
+                    cmd.Parameters.AddWithValue("@CCancha", t.CCancha.CCancha);
+                    cmd.Parameters.AddWithValue("@NumHoras", t.NumHoras);
+                    cmd.Parameters.AddWithValue("@MDescuento", t.MDescuento);
+                    cmd.Parameters.AddWithValue("@MTotal", t.MTotal);
+                    cmd.Parameters.AddWithValue("@FPagado", t.FPagado);
+                    cmd.Parameters.AddWithValue("@CHorario", t.CHorario.CHorario);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
 
         public bool Update(Alquiler t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("update Alquiler set CGrupo=@cgrupo,CCancha=@ccancha,NumHoras=@numhoras,MDescuento=@mdescuento,MTotal=@mtotal,FPagado=@fpagado,CHorario=@chorario",con);
+                    cmd.Parameters.AddWithValue("@cgrupo", t.CGrupo.CGrupo);
+                    cmd.Parameters.AddWithValue("@ccancha", t.CCancha.CCancha);
+                    cmd.Parameters.AddWithValue("@numhoras", t.NumHoras);
+                    cmd.Parameters.AddWithValue("@mdescuento", t.MDescuento);
+                    cmd.Parameters.AddWithValue("@mtotal", t.MTotal);
+                    cmd.Parameters.AddWithValue("@fpagado", t.FPagado);
+                    cmd.Parameters.AddWithValue("@chorario", t.CHorario.CHorario);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
     }
 }
--- Data/Implementacion/CalleRepository.cs
+++ Data/Implementacion/CalleRepository.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entity;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 namespace Data.Implementacion
 {
     public class CalleRepository : ICalleRepository
     {
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using(var con=new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("delete from Calle where id='" + id + "'", con);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
 
         public List<Calle> FindAll()
@@ -26,12 +43,46 @@ namespace Data.Implementacion
 
         public bool Insertar(Calle t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using(var con=new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("insert into Calle values(@NCalle,@CDistrito)");
+                    cmd.Parameters.AddWithValue("@NCalle", t.NCalle);
+                    cmd.Parameters.AddWithValue("@CDistrito", t.CDistrito.CDistrito);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
 
         public bool Update(Calle t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("update Calle set NCalle=@ncalle,CDistrito=@cdistrito)");
+                    cmd.Parameters.AddWithValue("@ncalle", t.NCalle);
+                    cmd.Parameters.AddWithValue("@cdistrito", t.CDistrito.CDistrito);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rpta;
         }
     }
 }
