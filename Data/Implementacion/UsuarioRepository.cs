1150c85a1992ac6c911e94b03c8113b88fa3da6d
Ernesto <johndoe@example.com>
2019-04-23 23:19:00 -0500

    mensaje

--- Data/Implementacion/UsuarioRepository.cs
+++ Data/Implementacion/UsuarioRepository.cs
@@ -55,12 +55,14 @@ namespace Data.Implementacion
                 var query = new SqlCommand("select u.cusuario, u.cdni, u.nusuario,u.numtelefono from usuario u where u.cusuario=@id",con);
                 using (var dr = query.ExecuteReader())
                 {
-                    query.Parameters.AddWithValue("@id", id);
-                    usuario_temp = new Usuario();
-                    usuario_temp.CUsuario = Convert.ToInt16(dr["cusuario"]);
-                    usuario_temp.CDNI = Convert.ToInt32(dr["cusuario"]);
-                    usuario_temp.NumTelefono = Convert.ToInt32(dr["numtelefono"]);
-                    usuario_temp.NUsuario = dr["nusuario"].ToString();
+                   
+                        query.Parameters.AddWithValue("@id", id);
+                        usuario_temp = new Usuario();
+                        usuario_temp.CUsuario = Convert.ToInt16(dr["cusuario"]);
+                        usuario_temp.CDNI = Convert.ToInt32(dr["cusuario"]);
+                        usuario_temp.NumTelefono = Convert.ToInt32(dr["numtelefono"]);
+                        usuario_temp.NUsuario = dr["nusuario"].ToString();
+                    
                 }
             }
             return usuario_temp;
