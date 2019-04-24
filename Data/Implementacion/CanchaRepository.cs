4ecb216e2a0c2de6c7c71fdfcf5ca103de32e633
Adrian <Adrian_Ames@outlook.com>
2019-04-24 02:39:11 -0500

    Avance implementación cancha

--- Data/Implementacion/CanchaRepository.cs
+++ Data/Implementacion/CanchaRepository.cs
@@ -36,7 +36,25 @@ namespace Data.Implementacion
 
         public List<Cancha> FindAll()
         {
-            throw new NotImplementedException();
+            var canchas = new List<Cancha>();
+
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+                    var query = new SqlCommand("", con);
+                    var dr = query.ExecuteReader();
+                    while (dr.Read())
+                    {
+                    }
+                }
+            } catch(Exception ex)
+            {
+                throw;
+            }
+
+            return canchas;
         }
 
         public Cancha FindById(int? id)
@@ -46,12 +64,60 @@ namespace Data.Implementacion
 
         public bool Insertar(Cancha t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+
+                    var query = new SqlCommand("insert into Cancha values (@CPropietario, @CCalle, @NCancha, @TDireccion, @MPrecioHora)", con);
+                    query.Parameters.AddWithValue("@CPropietario", t.CPropietario.NUsuario);
+                    query.Parameters.AddWithValue("@CCalle", t.CCalle.CCalle);
+                    query.Parameters.AddWithValue("@NCancha", t.NCancha);
+                    query.Parameters.AddWithValue("@TDireccion", t.TDireccion);
+                    query.Parameters.AddWithValue("@MPrecioHora", t.MPrecioHora);
+                    query.ExecuteNonQuery();
+
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return rpta;
         }
 
         public bool Update(Cancha t)
         {
-            throw new NotImplementedException();
+            bool rpta = false;
+
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["soccermatch"].ToString()))
+                {
+                    con.Open();
+
+                    var query = new SqlCommand("update Cancha set CPropietario = @cpropietario, CCalle = @ccalle, NCancha = @ncancha, TDireccion = @tdireccion, MPrecioHora = @mpreciohora)", con);
+                    query.Parameters.AddWithValue("@cpropietario", t.CPropietario.NUsuario);
+                    query.Parameters.AddWithValue("@ccalle", t.CCalle.CCalle);
+                    query.Parameters.AddWithValue("@ncancha", t.NCancha);
+                    query.Parameters.AddWithValue("@tdireccion", t.TDireccion);
+                    query.Parameters.AddWithValue("@mpreciohora", t.MPrecioHora);
+                    query.ExecuteNonQuery();
+
+                    rpta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return rpta;
         }
     }
 }
