2e5d9599e52bba7a26ad351bf12b94552503e747
Adrian <Adrian_Ames@outlook.com>
2019-04-28 01:45:56 -0500

    Agregando create de ciudad, departemnto y usuario

--- TP/Controllers/CiudadController.cs
+++ TP/Controllers/CiudadController.cs
@@ -1,5 +1,6 @@
 ﻿using Business;
 using Business.Implementacion;
+using Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,36 @@ namespace TP.Controllers
     public class CiudadController : Controller
     {
         private ICiudadService objCiudadService = new CiudadService();
+        private IDepartamentoService objDepartamentoService = new DepartamentoService();
         // GET: Ciudad
         public ActionResult Index()
         {
             return View(objCiudadService.FindAll());
+        }
+        // GET: Ciudad/Create
+        public ActionResult Create()
+        {
+            ViewBag.departamento = objDepartamentoService.FindAll();
+            return View();
+        }
+
+        // POST: Ciudad/Create
+        [HttpPost]
+        public ActionResult Create(Ciudad ciudad)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ViewBag.departamento = objDepartamentoService.FindAll();
+
+                bool rpta = objCiudadService.Insertar(ciudad);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
