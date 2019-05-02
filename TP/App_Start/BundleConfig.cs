d293103e6da961db04e96c103510f0d66e4db2f6
Adrian <Adrian_Ames@outlook.com>
2019-05-02 10:53:07 -0500

    Estilos 2.0

--- TP/App_Start/BundleConfig.cs
+++ TP/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace TP
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-theme.css"/*,
-                      "~/Content/site.css"*/));
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/site.css"));
         }
     }
 }
