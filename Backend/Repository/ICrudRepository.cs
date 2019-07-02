abecaab006c0975732bc0ebb0d2d27a914b0b2aa
ernestosanchezmatos <ernesto.sm.95@hotmail.com>
2019-07-02 16:21:10 -0500

    actualizando7

---
+++ Backend/Repository/ICrudRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface ICrudRepository<T>
+    {
+        bool Save(T entity);
+        bool Update(T entity);
+        bool Delete(int id);
+        IEnumerable<T> GetAll();
+        T Get(int id);
+    }
+}
---
+++ Backend/Service/Implementacion/EquipoService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Repository;
+using Repository.Implementacion;
+
+namespace Service.Implementacion
+{
+    public class EquipoService : IEquipoService
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Equipo Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Equipo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Equipo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Equipo entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
