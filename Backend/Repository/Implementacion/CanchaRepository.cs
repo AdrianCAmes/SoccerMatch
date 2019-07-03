bb05d726a3f98c5138e13f367569348ce8874db7
ernestosanchezmatos <ernesto.sm.95@hotmail.com>
2019-07-03 00:58:35 -0500

    act8

---
+++ Backend/Repository/Implementacion/CanchaRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Repository.Implementacion
+{
+    public class CanchaRepository : ICanchaRepository
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cancha Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cancha> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Cancha entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Cancha entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Service/Implementacion/CiudadService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Repository;
+using Repository.Implementacion;
+
+namespace Service.Implementacion
+{
+    public class CiudadService : ICiudadService
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ciudad Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ciudad> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Ciudad entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Ciudad entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
