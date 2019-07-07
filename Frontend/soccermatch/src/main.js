63b1047c38f80dff59c13c158348677fa0659002
MrtNg <marting_ng_96@hotmail.com>
2019-07-07 08:01:00 -0500

    Router

--- Frontend/soccermatch/src/main.js
+++ Frontend/soccermatch/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-
 Vue.config.productionTip = false
 axios.defaults.baseURL='https://localhost:5001/'
 
