12185a04b9517ab75a4c1f23e5621e1effd36f2d
Ernesto <ernesto.sm.95@hotmail.com>
2019-07-07 01:00:53 -0500

    REsubiendo

---
+++ Frontend/soccermatch/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      fixed
+      :clipped="$vuetify.breakpoint.mdAndUp"
+      app
+      v-model="drawer"
+    >         
+     
+    </v-navigation-drawer>
+    <v-toolbar
+      color="blue darken-3"
+      dark
+      app
+      :clipped-left="$vuetify.breakpoint.mdAndUp"
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">SoccerMatch</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+
+  
+  </v-app>
+</template>
+
+
+<script>
+
+import axios from "axios";
+export default {
+  name: 'App',
+  data () {
+    return {
+    
+      drawer: true,
+     }
+  },
+}
+</script>
