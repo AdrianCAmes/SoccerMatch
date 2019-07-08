ee39742ffdc52c7dc603ede0a7055d496b2b530d
MrtNg <marting_ng_96@hotmail.com>
2019-07-08 06:23:50 -0500

    P

--- Backend/Service/ICanchaService.cs
+++ Backend/Service/ICanchaService.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Repository.ViewModel;
 namespace Service
 {
     public interface ICanchaService : ICrudService<Cancha>
     {
+                IEnumerable<CanchaViewModel> ListarTodos();
     }
 }
---
+++ Backend/SoccerMatch/Controllers/CanchaController.cs
@@ -0,0 +1,51 @@
+using Entity;
+using Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerMatch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CanchaController : ControllerBase
+    {
+
+        private ICanchaService CanchaService;
+
+        public CanchaController(ICanchaService CanchaService)
+        {
+            this.CanchaService = CanchaService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(
+                CanchaService.ListarTodos()
+            );
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Cancha Cancha)
+        {
+            return Ok(
+                CanchaService.Save(Cancha)
+            );
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Cancha Cancha)
+        {
+            return Ok(
+                CanchaService.Update(Cancha)
+            );
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok(
+                CanchaService.Delete(id)
+            );
+        }
+
+    }
+}
